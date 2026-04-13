c7379ef3ce01a128b3f28f4f2063aa0f98f67a8d
SergioDomingos1965 <sebastiaofranciscodomingos1974@gmail.com>
2026-04-13 13:00:18 -0400

    feito

---
+++ Progresso_Web/aula_03/math.js
@@ -0,0 +1,7 @@
+/*
+    Math -> matematica 
+        - pow (2, 2) / potencia / 2 ^2 = 4
+*/
+
+const calculo = Math.pow(2, 4)
+console.log(calculo)
---
+++ Progresso_Web/aula_04/array.js
@@ -0,0 +1,6 @@
+const numeros = [1, 2 , 3 , 4 , 5]
+
+const dobro = numeros.map((numero) =>{ 
+    return numero * 2
+
+} ) 
